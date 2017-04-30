06c704737f081d957d7ec94c8a063382df41e4e0
Alexpham688 <Alexpham688@gmail.com>
2017-04-29 21:05:19 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+
+ // $('button').on('click',function(){
+ //   var search = $('#search').val();
+ //   searchReddit(search);
+ // });
+  // function searchReddit(redditData){
+$.get('http://www.reddit.com/r/food/.json').done(function(response){
+  console.log(response.data.children[0].data);
+//
+ // var reddits =['author','thumbnail',]
+  for(i = 0; i < response.data.length; i++){
+    var author = response.data.children[i].data.author;
+    var thumbnail = response.data.children[i].data.thumbnail;
+    var score = response.data.children[i].data.score;
+    $('body').append("<div><p>" + author + " </p></div>");
+  }
+});
+});
+// });
+// });
+// });
